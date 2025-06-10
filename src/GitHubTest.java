3f3aac5fc62917dc694c671c5c08b70fe4088a24
MayMePhone <maymephone249@gmail.com>
2025-06-10 11:06:49 +0630

    java file changed

--- src/GitHubTest.java
+++ src/GitHubTest.java
@@ -1,5 +1,8 @@
 public class GitHubTest {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        String name="May Me";
+        System.out.println("Your name is "+name);
     }
 }
